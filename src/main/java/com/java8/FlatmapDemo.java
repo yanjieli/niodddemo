a28e42baa429116f549e5768b22e43d6aca5dff7
yanjieli <opflyjie@126.com>
2019-12-10 22:33:14 +0800

    okkk

--- src/main/java/com/java8/FlatmapDemo.java
+++ src/main/java/com/java8/FlatmapDemo.java
@@ -6,9 +6,9 @@ import java.util.stream.Stream;
 public class FlatmapDemo {
 
 	public static void main(String[] args) {
-		String data[][] = new String[][] { { "1", "2" }, { "3", "4" } };
+		String data[][] = new String[][] { { "1", "2", "3" }, { "3", "4" } };
 		Stream<String[]> temp = Arrays.stream(data);
-		temp.filter(x -> x.length > 0);
+//		temp.filter(x -> x.length > 0);
 		Stream<String> stringStream = temp.flatMap(x -> Arrays.stream(x));
 		stringStream.filter(x -> Integer.parseInt(x) > 2).forEach(System.out::println);
 	}
